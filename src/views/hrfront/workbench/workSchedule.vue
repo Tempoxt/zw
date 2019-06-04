861b31f7c928a3bd7c9a5e0655928a592d1d710c
514994385@qq.com <514994385@qq.com>
2019-06-04 16:51:19 +0800

    添加 工作台

---
+++ src/views/hrfront/workbench/workSchedule.vue
@@ -0,0 +1,68 @@
+<template>
+	<el-card class="box-card">
+	  <div slot="header" class="clearfix">
+		<span class="cardName">工作日程</span>
+		<!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+	  </div>
+		
+       <el-row>
+			<el-col :span="14">
+				<div class="list-l">
+                    <el-calendar v-model="value">
+                    </el-calendar>
+				</div>
+			</el-col>
+			<el-col :span="10">
+				<div class="list-r">
+                    <div class="list-r-h">
+                        <span>日程表</span>
+                        <el-button icon="el-icon-plus"  size="mini"></el-button>
+                    </div>
+                    <div class="list-r-b">
+                        
+                    </div>
+				</div>
+			</el-col>
+		</el-row>
+	</el-card>
+	
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        value: new Date()
+      }
+    }
+}
+</script>
+<style>
+.list-r-h .el-button--mini{
+      padding: 7px 7px !important;
+      float: right;
+}
+</style>
+
+<style lang="scss" scoped>
+.box-card{
+	height: 350px;
+    margin-left: 10px;
+}
+.cardName{
+	color: #37474F;
+	font-size: 16px;
+	font-weight: bold
+}
+.list-l{
+    height: 240px;
+    border-right: 1px solid rgba(228,228,228,1);
+}
+.list-r{
+    padding-left: 10px;
+    .list-r-h{
+        font-size: 16px;
+        font-weight: bold;
+    }
+}
+</style>
