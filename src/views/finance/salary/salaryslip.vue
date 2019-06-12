d891e826ee56c0f35c372936dcb7e0f009cc25c0
syusuke <15737951003@163.com>
2019-06-12 17:25:43 +0800

    离职登记和薪资单

---
+++ src/views/finance/salary/salaryslip.vue
@@ -0,0 +1,36 @@
+<template>
+
+ <el-row class="h-full">
+    <el-col :span="5" class="h-full">
+      <div class=" h-full">
+           <org v-model="orgid" @change="changeOrg"/>
+      </div>
+
+    </el-col>
+    <el-col :span="19">
+        <salaryslipTable :id="orgid" />
+    </el-col>
+  </el-row>
+</template>
+<script>
+import org from '@/views/public/org'
+import salaryslipTable from './salaryslipTable'
+export default {
+    components:{
+        salaryslipTable,
+        org
+    },
+    data(){
+        return {
+            activeName:'first',
+            orgid:''
+        }
+    },
+    methods:{
+        changeOrg(orgid){
+            this.orgid = orgid
+        }
+    },
+}
+</script>
+
