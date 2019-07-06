63a7d713656d17ab8d21f7434b920434b4426b57
syusuke <15737951003@163.com>
2019-07-06 18:11:39 +0800

    修改密码

---
+++ src/views/account/editPassword.vue
@@ -0,0 +1,117 @@
+<template>
+	<div>
+		<el-dialog
+			title="修改密码"
+			:visible.sync="dialogFormVisible"
+			class="public-dialog"
+			v-el-drag-dialog  
+			>
+			<div>
+				<el-form ref="editForm" :model="editForm" label-width="100px" :rules="editRules">
+					<el-row :gutter="20">
+						<el-col :span="24">
+							<el-form-item label="密码" prop="password">
+								<el-input v-model="editForm.password" placeholder="请输入原密码"></el-input>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="新密码" prop="passwordNew">
+								<el-input v-model="editForm.passwordNew" placeholder="请输入新密码"></el-input>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="新密码验证" prop="passwordPard">
+								<el-input v-model="editForm.passwordPard" placeholder="请再次输入新密码"></el-input>
+							</el-form-item>
+						</el-col>
+					</el-row>
+				</el-form>
+			</div>
+
+			<div slot="footer" class="dialog-footer">
+				<el-button @click="dialogFormVisible = false">取 消</el-button>
+				<el-button type="primary" @click="handleFormSubmit" :disabled="disabeld">确 定</el-button>
+			</div>
+
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+import * as api_common from "@/api/common";
+export default {
+	props:['showModal'],
+	data() {
+		var validatePass = (rule, value, callback) => {
+			if (value === '') {
+				callback(new Error('请输入新密码'));
+			} else {
+			if (this.ruleForm.checkPass !== '') {
+				this.$refs.editForm.validateField('checkPass');
+			}
+			callback();
+			}
+		};
+		var validatePass2 = (rule, value, callback) => {
+			if (value === '') {
+				callback(new Error('请再次输入密码'));
+			} else if (value !== this.editForm.passwordNew) {
+				callback(new Error('两次输入密码不一致!'));
+			} else {
+				callback();
+			}
+		};
+		return {
+			editForm: {
+				password: '',
+				passwordNew: '',
+				passwordPard: '',
+			},
+			// dialogFormVisible:false,
+			editRules: {
+				password: [
+					{ required: true, trigger: "blur", message:'请输入原密码' }
+				],
+				passwordNew: [
+					{ required: true, trigger: "blur", validator: validatePass, }
+				],
+				passwordPard: [
+					{ required: true, trigger: "blur", validator: validatePass2, }
+				]
+			}
+		};
+	},
+	computed:{
+		dialogFormVisible:{
+			get(){
+                return this.showModal
+            },
+            set(val){
+                this.$emit('update:showModal',val)
+            }
+		},
+		disabeld(){
+            console.log(this.editForm,'hhh')
+            if(this.editForm.password==''||this.editForm.passwordNew==''||this.editForm.passwordPard==''||(this.editForm.passwordNew!=this.editForm.passwordPard)){
+                return true
+            }else{
+                console.log('555')
+                return false
+            }
+        }
+	},
+	methods:{
+		async handleFormSubmit(){
+			console.log(this.editForm);
+			this.form = await this.$request.post('/user/updatePwd',{
+				oldPwd:this.editForm.password,
+				newPwd:this.editForm.passwordNew
+			})
+		}
+	},
+	created(){
+		console.log(this.showModal,'visible')
+	}
+}
+</script>
+
--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -1,61 +1,79 @@
 <template>
-  <div class="navbar">
-    <!-- <breadcrumb class="breadcrumb-container"/> -->
-    <div class="nav-menu">
-      <hamburger
-        :toggle-click="toggleSideBar"
-        :is-active="sidebar.opened"
-        class="hamburger-container"
-      />
-      <nav-menu/>
-    </div>
-    <div class="right-menu">
-      <span class="right-menu-item">
-         <el-tooltip class="item" effect="dark" content="搜索" placement="bottom">
-              <i class="icon iconfont icon-sousuo1"></i>
-        </el-tooltip>
-      </span>
-      <span class="right-menu-item message-item">
-          <el-tooltip class="item" effect="dark" content="消息" placement="bottom">
-                  <i class="icon iconfont icon-xiaoxi"></i>
-                <span class="count">99</span>
-          </el-tooltip>
-      </span>
-      <span class="right-menu-item">
-        <el-tooltip class="item" effect="dark" content="手机" placement="bottom">
-              <i class="icon iconfont icon-shouji"></i>
-        </el-tooltip>
-      </span>
-      <span class="right-menu-item" @click="handleScreenfull">
-        <el-tooltip class="item" effect="dark" content="全屏" placement="bottom">
-             <i class="icon iconfont icon-quanping"></i>
-        </el-tooltip>
-      </span>
-      <template v-if="device!=='mobile'">
-        <el-tooltip :content="$t('navbar.screenfull')" effect="dark" placement="bottom"></el-tooltip>
+<div>
+	<div class="navbar">
+   		<!-- <breadcrumb class="breadcrumb-container"/> -->
+		<div class="nav-menu">
+			<hamburger
+				:toggle-click="toggleSideBar"
+				:is-active="sidebar.opened"
+				class="hamburger-container"
+			/>
+			<nav-menu/>
+		</div>
+		<div class="right-menu">
+			<span class="right-menu-item">
+				<el-tooltip class="item" effect="dark" content="搜索" placement="bottom">
+					<i class="icon iconfont icon-sousuo1"></i>
+				</el-tooltip>
+			</span>
+			<span class="right-menu-item message-item">
+				<el-tooltip class="item" effect="dark" content="消息" placement="bottom">
+					<i class="icon iconfont icon-xiaoxi"></i>
+					<span class="count">99</span>
+				</el-tooltip>
+			</span>
+			<span class="right-menu-item">
+				<el-tooltip class="item" effect="dark" content="手机" placement="bottom">
+					<i class="icon iconfont icon-shouji"></i>
+				</el-tooltip>
+			</span>
+			<span class="right-menu-item" @click="handleScreenfull">
+				<el-tooltip class="item" effect="dark" content="全屏" placement="bottom">
+					<i class="icon iconfont icon-quanping"></i>
+				</el-tooltip>
+			</span>
+			<template v-if="device!=='mobile'">
+				<el-tooltip :content="$t('navbar.screenfull')" effect="dark" placement="bottom"></el-tooltip>
 
-        <el-tooltip :content="$t('navbar.size')" effect="dark" placement="bottom"></el-tooltip>
-        <el-tooltip :content="$t('navbar.theme')" effect="dark" placement="bottom"></el-tooltip>
-      </template>
+				<el-tooltip :content="$t('navbar.size')" effect="dark" placement="bottom"></el-tooltip>
+				<el-tooltip :content="$t('navbar.theme')" effect="dark" placement="bottom"></el-tooltip>
+			</template>
 
-      <el-dropdown class="avatar-container right-menu-item" trigger="click">
-        <div class="avatar-wrapper">
-          <img src="http://cdn.admui.com/demo/pjax/2.0.0/images/avatar.svg" class="user-avatar">
-          <!-- <span class="user-name">林夏</span> -->
-        </div>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>{{userInfo.real_name||'暂无名称'}}</el-dropdown-item>
-          <el-dropdown-item @click.native="toggleStatus">{{status==2?'前端用户中心':'后端应用中心'}}</el-dropdown-item>
-          <!-- <router-link to="/">
-            <el-dropdown-item>{{ $t('navbar.dashboard') }}</el-dropdown-item>
-          </router-link> -->
-          <el-dropdown-item divided>
-            <span style="display:block;" @click="logout">退出</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </div>
-  </div>
+			<el-dropdown class="avatar-container right-menu-item" trigger="click">
+				<div class="avatar-wrapper">
+				<img src="http://cdn.admui.com/demo/pjax/2.0.0/images/avatar.svg" class="user-avatar">
+				<!-- <span class="user-name">林夏</span> -->
+				</div>
+				<el-dropdown-menu slot="dropdown">
+				<el-dropdown-item>{{userInfo.real_name||'暂无名称'}}</el-dropdown-item>
+				<el-dropdown-item @click.native="toggleStatus">{{status==2?'前端用户中心':'后端应用中心'}}</el-dropdown-item>
+				<el-dropdown-item @click.native="editPassword">修改密码</el-dropdown-item>
+				<!-- <router-link to="/">
+					<el-dropdown-item>{{ $t('navbar.dashboard') }}</el-dropdown-item>
+				</router-link>  @click="handleFormSubmit"-->
+				<el-dropdown-item divided>
+					<span style="display:block;" @click="logout">退出</span>
+				</el-dropdown-item>
+				</el-dropdown-menu>
+			</el-dropdown>
+		</div>
+	</div>
+  
+	<!-- <el-dialog
+		title="修改密码"
+		:visible.sync="dialogFormVisible"
+		class="public-dialog"
+		v-el-drag-dialog
+		> -->
+		<editPassword :showModal.sync="dialogFormVisible"/>
+
+		<!-- <div slot="footer" class="dialog-footer">
+			<el-button @click="dialogFormVisible = false">取 消</el-button>
+			<el-button type="primary" @click="handleFormSubmit">确 定</el-button>
+		</div>
+	</el-dialog> -->
+</div>
+ 
 </template>
 
 <script>
@@ -64,37 +82,53 @@ import Breadcrumb from "@/components/Breadcrumb";
 import Hamburger from "@/components/Hamburger";
 import NavMenu from "./NavMenu";
 import screenfull from "screenfull";
+import editPassword from "../../account/editPassword.vue";
 
 export default {
-  components: {
-    Breadcrumb,
-    Hamburger,
-    NavMenu
-  },
-  computed: {
-    ...mapGetters(["sidebar", "name", "device"]),
-    ...mapState({
-      status: state => state.app.status,
-      userInfo:state=>state.user.userInfo
-    })
-  },
-  methods: {
-    handleScreenfull() {
-      screenfull.toggle();
-    },
-    toggleSideBar() {
-      this.$store.dispatch("toggleSideBar");
-    },
-    toggleStatus() {
-      this.$store.commit("toggleStatus");
-    },
-    logout() {
-      // this.$router.push("/account/login");
-      this.$store.dispatch("user/LogOut").then(() => {
-        location.reload(); // In order to re-instantiate the vue-router object to avoid bugs
-      });
-    }
-  }
+	components: {
+		Breadcrumb,
+		Hamburger,
+		NavMenu,
+		editPassword
+	},
+	data(){
+		return{
+			dialogFormVisible:false,
+			showModal:false,
+			form:{}
+		}
+	},
+	computed: {
+		...mapGetters(["sidebar", "name", "device"]),
+		...mapState({
+		status: state => state.app.status,
+		userInfo:state=>state.user.userInfo
+		})
+	},
+	methods: {
+		editPassword(){
+			this.dialogFormVisible = true
+			console.log('ssss')
+		},
+		handleFormSubmit(){
+			console.log(this.form,'fff----')
+		},
+		handleScreenfull() {
+			screenfull.toggle();
+		},
+		toggleSideBar() {
+			this.$store.dispatch("toggleSideBar");
+		},
+		toggleStatus() {
+			this.$store.commit("toggleStatus");
+		},
+		logout() {
+			// this.$router.push("/account/login");
+			this.$store.dispatch("user/LogOut").then(() => {
+				location.reload(); // In order to re-instantiate the vue-router object to avoid bugs
+			});
+		}
+	}
 };
 </script>
 
