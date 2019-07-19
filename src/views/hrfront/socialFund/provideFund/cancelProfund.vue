25cdb5d97fbedfc9626ab40eac0bca3b2c27d06c
syusuke <15737951003@163.com>
2019-07-19 15:16:17 +0800

    公积金模块

---
+++ src/views/hrfront/socialFund/provideFund/cancelProfund.vue
@@ -0,0 +1,73 @@
+<template>
+
+ <el-row class="h-full">
+    <el-col :span="5" class="h-full">
+      <div class=" h-full">
+           <org v-model="orgid" @change="changeOrg"/>
+      </div>
+
+    </el-col>
+    <el-col :span="19">
+        <!-- <addAccideTable :id="orgid" /> -->
+        <el-tabs v-model="view_activeName" class="table-tabs" ref="tabs" @tab-click="handleClick">
+            <el-tab-pane :label="item.name" :name="item.name" lazy v-for="item in menu" :key="item.id"></el-tab-pane>
+        </el-tabs>
+        <div v-if="view_activeName==='待注销'">
+            <cancelProfundTable insure_status="14" :id="orgid"/>
+        </div>
+        <div v-if="view_activeName==='待确认'">
+            <cancelProfundTable insure_status="15" :id="orgid"/>
+        </div>
+        <div v-if="view_activeName==='已注销'">
+            <cancelProfundTable insure_status="16" :id="orgid"/>
+        </div>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import org from '@/views/public/org'
+import { getTabs } from '@/api/common'
+import cancelProfundTable from './cancelProfundTable'
+export default {
+    components:{
+        cancelProfundTable,
+        org
+    },
+    watch:{
+        filterText(val) {
+            this.$refs.tree2.filter(val);
+        }
+    },
+    data(){
+        return {
+            activeName:'first',
+            orgid:'',
+            filterText:'',
+            view_activeName:'',
+            menu:[],
+        }
+    },
+    methods:{
+        handleChangeNode(val){
+            this.orgid = val.orgid
+        },
+        changeOrg(orgid){
+            this.orgid = orgid
+        },
+        filterNode(value, data) {
+            if (!value) return true;
+            return data.name && data.name.indexOf(value) !== -1;
+        },
+        handleClick(val){
+            
+        },
+    },
+    async created() {
+        const { menu } = await getTabs(this.$route.query.menuid)
+        this.menu = menu
+        this.view_activeName = menu[0].name;
+    }
+}
+</script>
+
+
---
+++ src/views/hrfront/socialFund/provideFund/cancelProfundTable.vue
@@ -0,0 +1,189 @@
+  <template>
+  <ui-table ref="table" 
+  :table_column="table_field" 
+  :table_query.sync="table_form.query"
+  @query="querySubmit"
+  
+  >
+
+  		<el-dialog
+			title="公积金注销申请退回"
+			:visible.sync="dialogFormVisible"
+			class="public-dialog"
+			v-el-drag-dialog
+			width="520px"
+			>
+
+			<el-form ref="form" :model="form" label-width="100px" :inline="true">
+				<el-row>
+					<el-col :span="24" style="padding:20px;">
+						<el-input v-model="form.reason" placeholder="请输入退回原因（必填）"  size="small"></el-input>
+					</el-col>
+				</el-row>
+			</el-form>
+
+			<div slot="footer" class="dialog-footer">
+				<el-button @click="dialogFormVisible = false">取 消</el-button>
+				<el-button type="primary" @click="handleFormSubmit" :disabled="disabled">确 定</el-button>
+			</div>
+		</el-dialog>
+
+    <table-header
+		:table_actions="table_actions"
+		:table_selectedRows="table_selectedRows"
+		@action="handleAction"
+		:table_form.sync="table_form"
+		:table_column="table_field"
+		>
+		<div style="padding-left:10px" v-if="insure_status==15">
+          <dateLap v-model="table_form.dateLap" @change="fetchTableData"/>
+        </div>
+    </table-header>
+    <el-table
+      @selection-change="handleChangeSelection"
+      :data="table_data"
+      border
+      style="width: 100%"
+      v-loading="table_loading"
+      :header-cell-style="headerCellStyle"
+      :height="table_height"
+      @header-dragend="table_dragend"
+      @sort-change="table_sort_change"
+    >
+    <el-table-column 
+      type="selection" 
+      width="60" 
+      class-name="table-column-disabled"
+      :selectable="table_disable_selected"
+      >
+      </el-table-column>
+    <el-table-column type="index" :index="indexMethod" width="70"/>
+    <each-table-column :table_field="table_field" :template="template"/>
+    </el-table>
+     <table-pagination 
+        :total="table_form.total" 
+        :pagesize.sync="table_form.pagesize"
+        :currentpage.sync="table_form.currentpage"
+        @change="fetchTableData"
+        :table_config="table_config"
+    />
+  </ui-table>
+</template>
+<script>
+import * as api_common from "@/api/common";
+import table_mixin from "@c/Table/table_mixin";
+import dateLap from '@/components/Table/DateLap'
+import dayjs from 'dayjs'
+const api_resource = api_common.resource("staffinsure/accumulationfund");
+export default {
+	mixins: [table_mixin],
+	props:['id','insure_status'],
+	components:{
+		dateLap
+	},
+	data() {
+		return {
+			loading: true,
+			api_resource,
+			orgCategory:[],
+			queryDialogFormVisible:true,
+			dialogFormVisible:false,
+      		table_topHeight:286,
+			template:{
+				staff__outDutyTime(column,row){
+					if(row.expire){
+						if(row.expire==1){
+							return <span style="color:#F47A24">{row.staff__outDutyTime}(需要处理)</span>
+						}else if(row.expire==2){
+							return <span style="color:#F2353C">{row.staff__outDutyTime}(急需处理)</span>
+						}else if(row.expire==3){
+							return <span style="color:#F2353C">{row.staff__outDutyTime}(逾期)</span>
+						}else{
+							return <span>{row.staff__outDutyTime}</span>
+						}
+					}else{
+						return <span>{row.staff__outDutyTime}</span>
+					}
+				}
+			},
+			form:{
+				reason:''
+			}
+		};
+	},
+	watch:{
+		id(){
+			this.fetchTableData()
+		},
+		insure_status(){
+			this.fetchMenu()
+			this.fetchTableData()
+		}
+	},
+	computed:{
+		disabled(){
+			if(this.form.reason!=''){
+                return false
+            }
+            return true
+		}
+	},
+	methods: {
+		async fetchTableData() {
+			if(!this.id){
+				return
+			}
+			this.table_loading = true;
+			this.table_form.org_id = this.id
+			this.table_form.insureStatus = this.insure_status
+			const {rows , total }= await api_resource.get(this.table_form);
+			this.table_data  = rows
+			this.table_form.total = total
+			setTimeout(() => {
+				this.table_loading = false;
+			}, 300);
+		},
+		async apply(){
+			let rows = this.table_selectedRows.map(row=>row.id)
+			await this.$request.put('staffinsure/applycancelinsure',{
+				ids:rows.join(','),
+				insureType:2
+			})
+			this.fetchTableData();
+		},
+		async pass(){
+			let rows = this.table_selectedRows.map(row=>row.id)
+			await this.$request.put('staffinsure/outpassinsure',{
+				ids :rows.join(','),
+				insureType: 2,
+			})
+			this.fetchTableData();
+		},
+		back(){
+			this.dialogFormVisible = true
+		},
+		async handleFormSubmit(){
+			let rows = this.table_selectedRows.map(row=>row.id)
+			await this.$request.put('staffinsure/outrejectinsure',{
+				ids :rows.join(','),
+				insureType: 2,
+				remark: this.form.reason
+			})
+			this.dialogFormVisible = false
+			this.fetchTableData();
+		},
+		async fetchMenu(){
+			const { field, action,table } = await api_common.menuInit("ReserveFund"+this.insure_status);
+			this.table_field = field;
+			this.table_actions = action;
+			this.table_config = table
+		}
+	},
+	async created() {
+		this.fetchMenu()
+		this.fetchTableData();
+	}
+};
+</script>
+
+
