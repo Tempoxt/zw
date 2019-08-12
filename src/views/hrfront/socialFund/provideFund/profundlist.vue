31e72c4fbf12fb79890f636db9ef85782c5004a8
syusuke <15737951003@163.com>
2019-08-12 11:58:27 +0800

    页面统一样式

--- src/views/hrfront/socialFund/provideFund/profundlist.vue
+++ src/views/hrfront/socialFund/provideFund/profundlist.vue
@@ -1,6 +1,6 @@
 <template>
    <el-row class="h-full">
-    <el-col :span="5" class="h-full">
+    <el-col :span="5" class="h-full" style="border-right:1px solid #e8e8e8">
       <div class=" h-full">
           <org @change="changeOrg" getApi="org/fundselect"/>
       </div>
--- src/views/hrfront/socialFund/socialSecurity/addSecurity.vue
+++ src/views/hrfront/socialFund/socialSecurity/addSecurity.vue
@@ -1,7 +1,7 @@
 <template>
 
  <el-row class="h-full">
-    <el-col :span="5" class="h-full">
+    <el-col :span="5" class="h-full" style="border-right:1px solid #e8e8e8">
       <div class=" h-full">
            <org v-model="orgid" @change="changeOrg" getApi="org/addsocialselect"/>
       </div>
