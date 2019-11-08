79cb699dea73db2c1b86a99825f04d4d6ed3c1a2
syusuke <15737951003@163.com>
2019-11-08 14:54:00 +0800

    修改部门预算明细左侧组织结构

--- src/views/finance/financialSheet/budgeReport.vue
+++ src/views/finance/financialSheet/budgeReport.vue
@@ -3,7 +3,7 @@
  <el-row class="h-full">
     <el-col :span="4" class="h-full" style="border-right:1px solid #e8e8e8">
         <div class=" h-full">
-            <org v-model="orgid" @change="changeOrg" />   
+            <org v-model="orgid" @change="changeOrg" getApi="org/tree"/>   
         </div>
     </el-col>
 
