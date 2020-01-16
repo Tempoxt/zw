eaa2618e1981f1eb945bcc7fde6aec2644fdfc0b
syusuke <15737951003@163.com>
2020-01-16 11:03:09 +0800

    修改翻页序号显示

--- src/views/hrfront/devicemanager/devicelistTable.vue
+++ src/views/hrfront/devicemanager/devicelistTable.vue
@@ -73,6 +73,7 @@
             :height="table_height"
             @resizable-change="table_dragend"
             @sort-change="table_sort_change"
+		    :seq-config="{seqMethod: VxeIndexMethod}"
             >
             <vxe-table-column 
                 type="selection" 
